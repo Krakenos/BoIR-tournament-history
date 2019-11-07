b8b85e0bee92ae91fda56a9281d6ef5f68f34600
Zamiell <j.nesta@gmail.com>
2019-11-07 13:20:12 -0500

    new tournaments and better scripts

--- audit.py
+++ audit.py
@@ -1,6 +1,7 @@
 # Imports
 import glob
 import json
+import re
 
 # Constants
 json_fields = [
@@ -47,28 +48,67 @@ for path in sorted(glob.glob('tournaments/*.json')):
         if not checkKey(tournament_json, field):
             print(path + ' - ' + field + ' does not exist!')
 
+    # Validate the winner field
+    if tournament_json['winner'] == '':
+        print(path + ' - winner field is empty.')
+
     # Check for player duplicates
     for matchup in tournament_json['matchups']:
         for racer in [matchup['winner'], matchup['loser']]:
+            # Validate null racers
+            if racer == '' or racer is None:
+                print(path + ' - Null racer detected.')
+                continue
+
+            # Validate test racers
+            if 'test' in racer:
+                print(path + ' - Test racer detected.')
+                continue
+            if 'placeholder' in racer:
+                print(path + ' - Placeholder racer detected.')
+                continue
+
             # Validate case stylization
             if racer not in racers and racer.lower() not in racers:
                 racers[racer] = True
             if racer not in racers and racer.lower() in racers:
                 print(path + ' - ' + racer + ' is styled incorrectly.')
 
+            # For the rest of the validation, use the lowercase version of the racer's name
+            original_racer = racer
+            racer = racer.lower()
+
             # Validate the amount of underscores
             racer_without_underscores = racer.replace('_', '')
             if racer_without_underscores not in racers2:
                 racers2[racer_without_underscores] = racer
             if racers2[racer_without_underscores] != racer:
-                print(path + ' - ' + racer + ' has the incorrect amount of underscores. ' + 
-                      '(It should be "' + racers2[racer_without_underscores] + '".)')
+                print(path + ' - ' + original_racer + ' has the incorrect amount of underscores.')
 
             # Validate the first X characters
             # (but skip team events since most teams will begin with "Team X")
             if tournament_json['ruleset'] != 'team':
-                racer_prefix = racer[:5]
+                duplicate_letters = 5
+                if 'bindingof' in racer:
+                    duplicate_letters = 10
+                racer_prefix = racer[:duplicate_letters]
                 if racer_prefix not in racers3:
                     racers3[racer_prefix] = racer
                 if racers3[racer_prefix] != racer:
                     print(path + ' - ' + racer + ' is potentially a duplicate of "' + racers3[racer_prefix] + '".')
+
+    # Check for anomalous scores
+    for matchup in tournament_json['matchups']:
+        if matchup['score'] == '0-0':
+            print(path + ' - ' + matchup['winner'] + ' vs ' + matchup['loser'] + ' has an score of "0-0".')
+            continue
+
+        match = re.search(r'^(\d+)-(\d+)$', matchup['score'])
+        if not match:
+            print(path + ' - ' + matchup['winner'] + ' vs ' + matchup['loser'] + ' has an invalid score of "' + matchup['score'] + '".')
+            continue
+
+        max_score = 15
+        if int(match.group(1)) >= max_score or int(match.group(2)) >= max_score:
+            print(path + ' - ' + matchup['winner'] + ' vs ' + matchup['loser'] + ' has a potentially bogus score of "' + matchup['score'] + '".')
+            continue
--- tournament_json_creator.py
+++ tournament_json_creator.py
@@ -1,7 +1,9 @@
+# Imports
+import dotenv
 import json
 import requests
+import sys
 import os
-import dotenv
 
 # Get the path of the script
 # From: https://stackoverflow.com/questions/4934806/how-can-i-find-scripts-directory-with-python
@@ -10,31 +12,48 @@ DIR = os.path.dirname(os.path.realpath(__file__))
 # Load the ".env" fle
 dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 API_KEY = os.environ.get('API_KEY')
-
+if API_KEY == '' or API_KEY is None:
+    print('Error: You must specify your Challonge API key in the ".env" file.')
+    sys.exit(1)
 
 def main():
+    # Check for a command line argument that specifies the tournament ID
+    if len(sys.argv) == 2:
+        get_tournament(sys.argv[1])
+        sys.exit(0)
+
     while True:
-        print('Type q to exit')
-        tourney_id = input('enter the tournament id:')
+        print('Type "q" to exit.')
+        tourney_id = input('Enter the Challonge tournament ID: ')
         if tourney_id == 'q':
             break
 
-        # Get the data for this tournament through the Challonge API
-        url = 'https://api.challonge.com/v1/tournaments/' + tourney_id + '.json'
-        params = {'api_key': API_KEY, 'include_participants': 1, 'include_matches': 1}
-        tourney = requests.get(url, params=params).json()
-        tourney_data = tourney['tournament']
-        date = tourney_data['started_at'].split('T', 1)[0]  # YYYY-MM-DD
-        json_file += ''.join(e for e in tourney_data['name'] if e not in '/\\?*"<>|:') + '.json'
-        output_path = path.join(DIR, 'tournaments', json_file)
-        parsed_json = json_parser(tourney_data, tourney_id, date)
-        with open(output_path, 'w', encoding='utf-8', newline='\n') as data_file:
-            json.dump(parsed_json, data_file, indent=2)
+        get_tournament(tourney_id)
 
 
-def json_parser(tournament, t_id, date):
-    matches = tournament['matches']
-    participants = tournament['participants']
+def get_tournament(tourney_id):
+    # Get the data for this tournament through the Challonge API
+    url = 'https://api.challonge.com/v1/tournaments/' + tourney_id + '.json'
+    params = {'api_key': API_KEY, 'include_participants': 1, 'include_matches': 1}
+    api_response = requests.get(url, params=params)
+    if api_response.status_code != 200:
+        print('Error: The Challonge API returned an HTTP response code of "' + str(api_response.status_code) + '".')
+        sys.exit(1)
+    tourney_json = api_response.json()
+    tourney_data = tourney_json['tournament']
+
+    # Parse it and create a new JSON object with just the data that we need
+    date = tourney_data['started_at'].split('T', 1)[0]  # YYYY-MM-DD
+    parsed_json = parse_json(tourney_data, tourney_id, date)
+
+    # Write it to a file
+    json_file = ''.join(e for e in tourney_data['name'] if e not in '/\\?*"<>|:') + '.json'
+    output_path = os.path.join(DIR, 'tournaments', json_file)
+    with open(output_path, 'w', encoding='utf-8', newline='\n') as data_file:
+        json.dump(parsed_json, data_file, indent=2)
+
+
+def parse_json(tournament, t_id, date):
     parsed_json = {'name': tournament['name'],
                    'challonge_id': t_id,
                    'challonge': tournament['full_challonge_url'],
@@ -44,16 +63,29 @@ def json_parser(tournament, t_id, date):
                    'ruleset': '',
                    'description': '',
                    'videos': [],
-                   'matchups': []
+                   'matchups': [],
+                   'winner': 'n/a'
                    }
+
+    matches = tournament['matches']
+    tournament_winner = ''
     for match in matches:
         match_data = match['match']
 
         # If there is no winner in the match data, then we want the score to be a draw
         if match_data['winner_id'] is None:
+            # Validate that there are player IDs
+            if ((match_data['player1_id'] == '' or match_data['player1_id'] is None) and
+                (match_data['player2_id'] == '' or match_data['player2_id'] is None)):
+
+               print('Error: Failed to find the player IDs for the following match: ' + match_data)
+               sys.exit(1)
+
+            # Since it was a tie, put a score of "0-0"
+            # This match should be investigated later and either deleted entirely or have manual score set
             parsed_json['matchups'].append({'winner': match_data['player1_id'],
                                             'loser': match_data['player2_id'],
-                                            'score': 'draw'})
+                                            'score': '0-0'})
 
         # If there is a winner, we get the score
         else:
@@ -62,28 +94,43 @@ def json_parser(tournament, t_id, date):
             try:
                 match_score = scores[0] + '-' + scores[1]
             except IndexError:
-                match_score = '3-0'   # scenario where match is forfeited
+                match_score = '0-0'  # Account for when a match is forfeited
             parsed_json['matchups'].append({'winner': match_data['winner_id'],
                                             'loser': match_data['loser_id'],
                                             'score': match_score})
 
+            # The tournament winner is not listed in the JSON that we get back from the Challonge API
+            # Thus, assume that the winner of the last match is the tournament winner
+            tournament_winner = match_data['winner_id']
+
+
     # Looping through participants to get their ID
+    participants = tournament['participants']
     for participant in participants:
         participant_id = participant['participant']['id']
         participant_group_id = participant['participant']['group_player_ids']
         participant_name = participant['participant']['name']
-        if participant_name == '':  # Sometimes name is empty because it's tied to Challonge account
+        if participant_name == '' or participant_name is None:  # Sometimes the name is empty because it is tied to the Challonge account
             participant_name = participant['participant']['challonge_username']
+        if participant_name == '' or participant_name is None:
+            print('Error: Was not able to get the name for participant:' + participant)
+            sys.exit(1)
 
         # Replace the ID with the player name in the parsed JSON
         for match in parsed_json['matchups']:
             winner = match['winner']
-            loser = match['loser']
             if winner == participant_id or winner in participant_group_id:
                 match['winner'] = participant_name
+
+            loser = match['loser']
             if loser == participant_id or loser in participant_group_id:
                 match['loser'] = participant_name
 
+        # Replace the tournament winner ID
+        if tournament_winner == participant_id:
+            tournament_winner = participant_name
+
+    parsed_json['winner'] = tournament_winner
     print('JSON parsed successfully.')
     return parsed_json
 
